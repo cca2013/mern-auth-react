b622ff6a25f9f190efd47076224e995ae65d0103
unknown <skafosbiz@gmail.com>
2021-03-13 10:17:42 +0200

    make it better

---
+++ client/build/precache-manifest.d0f3903e946eeb7c653553e79cab6b4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9a5f9d21902a0c2ff97",
+    "url": "/static/css/main.3a769e01.chunk.css"
+  },
+  {
+    "revision": "a9a5f9d21902a0c2ff97",
+    "url": "/static/js/main.a9a5f9d2.chunk.js"
+  },
+  {
+    "revision": "b92e1f38e875be59180e",
+    "url": "/static/js/1.b92e1f38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "20b43720be0a622a7bf55c0dc283a5aa",
+    "url": "/index.html"
+  }
+];
